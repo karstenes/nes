ecf8001bf58f39a5c98176719ebd7be2897a1d97
karstenes <keschnier@crimson.ua.edu>
2022-03-15 16:47:48 -0500

    file reading

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@
 /// 
 /// 
 
-use std::{thread, time};
+
+use std::io::Read;
 
 
 mod memory;
@@ -93,6 +94,30 @@ impl APU {
     }
 }
 
+#[allow(non_camel_case_types)]
+struct iNES {
+    // header
+    head: usize,
+    PGRSIZE: u8,
+    CHRSIZE: u8, // 0 = CHR RAM
+    flags6: u8,
+    flags7: u8,
+    flags8: u8,
+    flags9: u8,
+    flags10: u8,
+
+    // body
+    trainer: Vec<u8>,
+    PRG: Vec<u8>,
+    CHR: Vec<u8>
+}
+
+struct Game {
+    PRG_ROM: Vec<u8>,
+    CHR_ROM: Vec<u8>,
+
+}
+
 pub struct Console {
     CPU: CPU,
     PPU: PPU,
@@ -100,9 +125,37 @@ pub struct Console {
     Memory: memory::Memory
 }
 
+#[allow(non_snake_case)]
+fn read_iNES(path: String) -> Result<iNES, std::io::Error> {
+    let mut file = std::fs::File::open(path)?;
+    let mut rom: Vec<u8> = vec![0;1024];
+    file.read(&mut rom)?;
+    
+
+    /*for (pos, val) in rom[0..4].iter().cloned().enumerate() {
+        if val != (0x4e45531a >> 48-(8*pos)) as u8 {
+            panic!("bad header");
+        }
+    }*/
+
+    Ok( iNES {
+        head: 0x4e45531a,
+        PGRSIZE: rom[4],
+        CHRSIZE: rom[5],
+        flags6: rom[6],
+        flags7: rom[7],
+        flags8: rom[8],
+        flags9: rom[9],
+        flags10: rom[10],
+
+        trainer: vec![0;if rom[6] & 0x4 != 0 {8192} else {0}],
+        PRG: rom[(16+(if rom[6] & 0x4 != 0 {512} else {0}))..(16+(if rom[6] & 0x4 != 0 {512} else {0})+(rom[4] as usize*16384))].to_vec(),
+        CHR: rom[(16+(if rom[6] & 0x4 != 0 {512} else {0})+(rom[4] as usize*16384))..(16+(if rom[6] & 0x4 != 0 {512} else {0})+(rom[4] as usize*16384)+(rom[5] as usize * 8192))].to_vec()
+    })
+}
+
 fn main() {    
-    for _ in 1..10 {
-    println!("bruh");
-    thread::sleep(time::Duration::from_secs(1));
-    }
+    let dk = read_iNES("dk.nes".to_string()).expect("Read Error");
+
+    print!("PGRSIZE: {}\nCHRSIZE: {}\n", dk.PGRSIZE, dk.CHRSIZE);
 }
